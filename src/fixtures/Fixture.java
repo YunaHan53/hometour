8cc866e340e54338dc7d4b366c06422350efa4de
YunaH <YunaH503@gmail.com>
2021-05-27 00:00:14 -0400

    added more classes

--- src/fixtures/Fixture.java
+++ src/fixtures/Fixture.java
@@ -1,5 +1,15 @@
 package fixtures;
 
 public abstract class Fixture {
-
+	
+	protected String name;
+	protected String shortDescription;
+	protected String longDescription;
+	
+	public Fixture(String name, String shortDescription, String longDescription) {
+		super();
+		this.name = name;
+		this.shortDescription = shortDescription;
+		this.longDescription = longDescription;
+	}
 }
--- src/fixtures/Room.java
+++ src/fixtures/Room.java
@@ -1,5 +1,18 @@
 package fixtures;
 
-public class Room {
+public class Room extends Fixture {
 
+	
+	public Room(String name, String shortDescription, String longDescription) {
+		super(name, shortDescription, longDescription);
+		this.exits = new Room[4]; // size is your choice
+	}
+		
+	public Room[] getExits() {
+		
+	}
+		
+	public Room getExit(String direction) {
+		
+	}
 }
