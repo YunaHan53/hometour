54669e036eafd99267997bd7a86c6b8b97678d29
YunaH <YunaH503@gmail.com>
2021-05-28 02:04:21 -0400

    Implemented 4 rooms with player being able to move forward from room to room.

--- src/game/Main.java
+++ src/game/Main.java
@@ -7,33 +7,41 @@ public class Main {
 		
 	// Main method
 	public static void main(String[] args) {
-		// Run the init method to instantiate our house
+		
+		// Create new player instance
 		Player p1 = new Player();
+		
+		// Run the init method to instantiate our house
 		Room[] myFlat = RoomManager.init();
+
 		p1.setCurrentRoom(myFlat[0]);
 
 		if(p1.getIndex() == 0) {
-		System.out.println("You woke up this morning at 9:55am\nYou immediately realized that you need to get ready in 5 minutes.\n");
+		System.out.println("You woke up this morning at 9:55am\n"); 
+		System.out.println("You immediately realized that you need to get ready in 5 minutes.\n");
 		System.out.println(p1.getCurrentRoom().longDescription);
 		}
-		while(true) {
-			
-			
-			
-		}
 		
-		Scanner s = new Scanner(System.in);
-		String str = s.nextLine();
-		String[] tok = str.split("\\s+");
-
-		//go ____ options
-		if(tok[0].equals("Go") || tok[0].equals("go")) {
-			if(tok[1].equals("forward") || tok[1].equals("Forward")) {
+		while(true) {
+			Scanner s = new Scanner(System.in);
+			String str = s.nextLine();
+			String[] t = str.split("\\s+");
+			
+			
+			//go ____ options
+			if(t[0].equals("Go") || t[0].equals("go")) {
+				if(t[1].equals("forward") || t[1].equals("Forward")) {
+					goForward(p1,myFlat);
+				}else if(t[1].equals("back") || t[1].equals("Back")) {
+					goBack(p1,myFlat);
+				}else {
+					System.out.println("I dont think I can go that way");
+				}
+			}else if(t[0].equals("Forward") || t[0].equals("forward")) {
 				goForward(p1,myFlat);
-			}else if(tok[1].equals("back") || tok[1].equals("Back")) {
+			}else if(t[0].equals("Back") || t[0].equals("back")) {
 				goBack(p1,myFlat);
-			}else {
-				System.out.println("I dont think I can go that way");
+			s.close();
 			}
 		}
 
@@ -41,18 +49,14 @@ public class Main {
 
 		
 	public static void goForward(Player p1, Room[] myFlat) {
-		if(p1.getIndex() == 1) {
-			System.out.println("There's no door there");
-		}
+
 		p1.setIndex(p1.getIndex() +p1.getCurrentRoom().goForward );
 		p1.setCurrentRoom(myFlat[p1.getIndex()]);
 		System.out.println(p1.getCurrentRoom().longDescription);	
 	}
 	
 	public static void goBack(Player p1, Room[] myFlat) {
-		if(p1.getIndex() == 1) {
-			System.out.println("There's no door there");
-		}
+
 		p1.setIndex(p1.getIndex() +p1.getCurrentRoom().goBack );
 		p1.setCurrentRoom(myFlat[p1.getIndex()]);
 		System.out.println(p1.getCurrentRoom().longDescription);
--- src/game/RoomManager.java
+++ src/game/RoomManager.java
@@ -4,30 +4,56 @@ import fixtures.Room;
 public class RoomManager {
 
 	public static Room[] init() {
-		Room[] myFlat = new Room[3];
+		Room[] myFlat = new Room[4];
 		
 		Room startingRoom = new Room(
-				"-back",
-				"You are back in your bedroom\n You remember that you promised Jo that you'd give him a tour of your flat.",
-				"",
+				"You wake up again...\n",
+				"You are back in your bedroom\n",
+				"You remember that you promised Jo that you'd give him a virtual tour of your flat.\n"
+				+ "You prepared and got on a Zoom video call with Jo and you began the tour.\n"
+				+ "There are two windows behind you from where you are standing\n"
+				+ "and the only door that's in front of you exits to the hallway, go 'forward'?\n",
 				1,
 				2,
-				"The only door that's in front of you leads to the hallway, go forward?");
+				"");
 		myFlat[0] = startingRoom;
 
 		Room hallway = new Room(
-				"You walked out of your bedroom into the hallway\n",
-				"The hallway is bright and long and leads straight into the living room\n",
-				"On your left, there is a door that leads to the bathroom, but the door is locked right now\n",
+				"forward - ",
+				"Hallway",
+				"You walked out of your bedroom into the hallway.\n"
+				+ "The hallway is bright and long with navyblue carpet and white walls.\n"
+				+ "On your left, there is a door that leads to the bathroom, but the door is locked right now.\n"
+				+ "At the end of the hallway, you can see the large living room. Go 'forward'?\n",
 				1,
 				2,
-				"You can go 'forward' into the livingroom or go 'back' to your bedroom. What would you like to do?");
+				"");
 		myFlat[1] = hallway;
 		
-//		Room livingRoom = new Room(
-//				"My living room is large with floor-to-ceiling windows\n" +
-//				"It is sunny out today and the windows let in a lot of light that brightens up the entire flat.");
-//		myFlat[2] = livingRoom;
+		Room livingRoom = new Room(
+				"forward - ",
+				"Living Room",
+				"You decided to walk the length of the hallway into the living room.\n"
+				+ "The living room is large with floor-to-ceiling windows.\n"
+				+ "It is very sunny out today and the windows let in a lot of light that brightens up the entire flat."
+				+ "To the right of the living room, there is the kitchen/dining room. Go 'forward'?\n",
+				1,
+				2,
+				"");
+		myFlat[2] = livingRoom;
+		
+		Room kitchen = new Room(
+				"forward - ",
+				"Kitchen/dining room",
+				"You decided to take a right into the kitchen/dining room.\n"
+				+ "The kitchen is large with open space design and L-shaped layout.\n"
+				+ "On the left wall there are three large built-in pantry cabinets.\n"
+				+ "On the far right wall, there is a sliding glass door leading to the backyard.\n"
+				+ "This concludes the tour of my home. Thank you!",
+				1,
+				2,
+				"");
+		myFlat[3] = kitchen;
 		
 		return myFlat;
 	}
